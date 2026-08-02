995dd897f05ddfebc6f9d0347e95a94a65bfda59
Dinie Masjuki <diniemasjuki@gmail.com>
2026-08-02 15:19:31 +0800

    doctype edit

--- rpwp_custom_app/traveller_management/doctype/rooming_aroya/rooming_aroya_tree.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2026, RPWP Developer Team and contributors
-// For license information, please see license.txt
-
-// frappe.treeview_settings["Rooming Aroya"] = {
-// };
--- rpwp_custom_app/traveller_management/doctype/trip/trip.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, RPWP Developer Team and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Trip", {
-// 	refresh(frm) {
-
-// 	},
-// });
